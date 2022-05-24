ec34483630885844d1f755f84007bbe76aa3c3d5
Maya Morales <mayainomorales@gmail.com>
2022-05-23 20:56:52 -0700

    add reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -5,6 +5,27 @@ const reactionSchema = new Schema (
         reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
-        }
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
     }
-)
+);
+
+module.exports = reactionSchema;
